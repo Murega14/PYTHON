df34c418544a321d082c3daaf5ef20b57decfbc1
murega14 <tedmurega@gmail.com>
2022-12-31 11:14:02 +0300

    changes

--- helloWorld.py
+++ helloWorld.py
@@ -3,7 +3,8 @@ last = 'Murega'
 name = first + " " + last
 id = 3456
 age = 20
-print("Hello", name, age)
+height = 180.5
+print("Hello", name, "of age:", str(age), "height:", str(height), "in cm")
 #type is used to show the data type of a variable
 print(type(name))
 print(type(id))
