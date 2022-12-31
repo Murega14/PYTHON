778b3a61c0509378079f1f3c732feacc22adeee1
murega14 <tedmurega@gmail.com>
2022-12-31 12:15:06 +0300

    math functions

---
+++ math_functions.py
@@ -0,0 +1,14 @@
+#import first the math module to make math functions available
+import math
+
+pi =3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi, 2))
+#basically all these functions and many others are available due to the math module
+
+x, y, z = 20, 134, 3
+print(max(x, y, z))
+print(min(x, y, z))
