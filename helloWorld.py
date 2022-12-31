93ec5904c6f1f173bc9a27fae1f36d8791cf30a0
murega14 <tedmurega@gmail.com>
2022-12-31 11:20:13 +0300

    modifications

--- helloWorld.py
+++ helloWorld.py
@@ -4,9 +4,12 @@ name = first + " " + last
 id = 3456
 age = 20
 height = 180.5
+human = True
 #use str() to help use different data types with a string
 print("Hello", name, "of age:", str(age), "height:", str(height), "in cm")
+print("Are you a human?", str(human))
 #type is used to show the data type of a variable
 print(type(name))
 print(type(id))
 print(type(height))
+print(type(human))
