89a7263242e1f4be2828de96eec0e49b18b80713
murega14 <tedmurega@gmail.com>
2022-12-31 11:56:15 +0300

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,12 @@
+#typecasting allows you to convert the data type of a value to another data type
+x = 1 #integer
+y = 2.0 #float
+z = "3" #string
+print(z)
+
+#changing y to int
+print(int(y))
+#changing z to int
+print(int(z))
+#change x to float
+print(float(x))
