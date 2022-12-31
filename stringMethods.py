2963156b46ee1d9ea224f172772f50158b496904
murega14 <tedmurega@gmail.com>
2022-12-31 11:38:26 +0300

    string methods

---
+++ stringMethods.py
@@ -0,0 +1,21 @@
+name = "Bravin Murega"
+#prints the length of the whole length
+print(len(name))
+#shows the position of the character specified
+print(name.find("u"))
+#capitalizes the first letter of string
+print(name.capitalize())
+#changes all characters to uppercase
+print(name.upper())
+#changes all characters to lowercase
+print(name.lower())
+#checks if variable is a digit
+print(name.isdigit())
+#checks if variable is alphabetic, if there is a space it will return false 
+print(name.isalpha())
+#counts the number of the character specified
+print(name.count("a"))
+#replaces selected character with specified character
+print(name.replace("a","o"))
+#prints the name variable according to the number specified
+print(name*3)
