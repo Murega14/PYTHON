8c3b42aed6d5a5bc16f877dbce1ad3d380c8db14
murega14 <tedmurega@gmail.com>
2023-01-03 11:52:53 +0300

    2D lists in python

---
+++ 2D_lists.py
@@ -0,0 +1,13 @@
+#a list of separate lists
+
+gin = ["gilbeys", "best", "gordons"]
+whiskey = ["all seasons", "red label", "vat 69"]
+rum = ["captain morgan", "konyagi"]
+
+liqour = [gin, whiskey, rum]
+print(liqour)
+
+#to print say red label
+print(liqour[1][1])
+#to print konyagi
+print(liqour[2][1])
