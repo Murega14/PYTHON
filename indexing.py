1d16b2c23e40bf891ba9ee7a838a42f3786c2059
murega14 <tedmurega@gmail.com>
2023-01-03 12:58:06 +0300

    indexing

---
+++ indexing.py
@@ -0,0 +1,8 @@
+#an index operator[] gives access to a sequences element(str, list, tuple)
+
+name = "bravin murega"
+
+if(name[0].islower() and name[7].islower()):
+    name = name.capitalize()
+
+print(name)
