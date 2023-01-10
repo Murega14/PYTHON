6b645b841dab4605f5ebfb67024a5ea29cc8e2e6
murega14 <tedmurega@gmail.com>
2023-01-10 21:28:07 +0300

    keyword arguments in python

---
+++ keyword_arguments.py
@@ -0,0 +1,9 @@
+#keyword arguments are arguments preceeded by an identifier when we pass themto  a function
+"""
+    the order of the arguments does not matter
+    python knows the names of the arguments that the function receives"""
+
+def hello(first, last):
+    print("hello ", first, last)
+
+hello(last="murega", first="Bravin")
