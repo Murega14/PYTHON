a93b02fcf15195876702542a1f19709cef3cb153
murega14 <tedmurega@gmail.com>
2023-01-10 21:21:28 +0300

    return statements in python

---
+++ return_statements.py
@@ -0,0 +1,7 @@
+#a return statement is a function that sends python values/objects back to the caller
+#the values/objects are known as the function's return value
+
+def mul(num1, num2):
+    return num1 * num2
+
+print(mul(25, 899))
