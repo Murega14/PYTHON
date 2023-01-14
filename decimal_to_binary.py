fb495cb0035c72d5ae90cc763225f508b52c1aae
murega14 <tedmurega@gmail.com>
2023-01-14 21:18:31 +0300

    decimal number to binary converter

---
+++ decimal_to_binary.py
@@ -0,0 +1,3 @@
+number = input("enter the number you would like to convert: ")
+binary = int(number)
+print("The number in binary is {:b}".format(binary))
---
+++ string_format.py
@@ -0,0 +1,7 @@
+#str.format(value) is a method that gives users more control when displaying output
+
+noun = 'Kenyans'
+verb = 'drinking'
+
+text = "{} love {}"
+print(text.format(noun, verb))
