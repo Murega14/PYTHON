7a778a910e28cc144363e7b80bc5b1c456097e7f
murega14 <tedmurega@gmail.com>
2023-01-16 09:35:31 +0300

    object oriented programming

--- car.py
+++ car.py
@@ -2,3 +2,7 @@ from object_oriented_programming import Car
 
 car_1 = Car("Ford", "Mustang", 2015, "red")
 print(car_1.model, car_1.make, car_1.year, car_1.color)
+print(Car.wheels)
+
+#if i want to change the class variable i would;
+#Car.wheels = 6
--- object_oriented_programming.py
+++ object_oriented_programming.py
@@ -1,6 +1,9 @@
 class Car():
+    
+    wheels = 4 #class variable
+
     def __init__(self, model, make, year, color):
-        self.model = model
+        self.model = model   #this is an instance variable
         self.make = make
         self.year = year
         self.color = color
