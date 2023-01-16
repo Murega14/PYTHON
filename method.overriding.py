d4c2ffcf8850dde2e807e2da428b693a6b8e02b2
murega14 <tedmurega@gmail.com>
2023-01-16 10:33:33 +0300

    method overriding in python

---
+++ method.overriding.py
@@ -0,0 +1,11 @@
+class Animal:
+    def eat(self):
+        print("this animal is eating")
+
+class Rabbit():
+
+    def eat(self):
+        print("this rabbit is eating a carrot")
+
+rabbit = Rabbit()
+rabbit.eat()
