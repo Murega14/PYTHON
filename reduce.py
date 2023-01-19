0f7acce3946cfd659953b417ccfe3a384f5c855e
murega14 <tedmurega@gmail.com>
2023-01-19 10:46:59 +0300

    reduce function in python

--- reduce.py
+++ reduce.py
@@ -7,3 +7,7 @@ import functools
 letters = ["h", "e", "l", "l", "o"]
 word = functools.reduce(lambda x, y:x + y, letters)
 print(word)
+
+factorial = [5,4,3,2,1]
+result = functools.reduce(lambda x,y:x * y, factorial)
+print(result)
